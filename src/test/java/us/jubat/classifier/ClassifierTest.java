3a1a6d713638d8c318daec702e6a0c429431c2e7
UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
2012-04-13 20:09:59 +0900

    independent test removed

--- src/test/java/us/jubat/classifier/ClassifierTest.java
+++ src/test/java/us/jubat/classifier/ClassifierTest.java
@@ -3,27 +3,10 @@ package us.jubat.classifier;
 import org.junit.*;
 
 public class ClassifierTest {
-    /*
-    public static void main(String[] args){
-	try{
-	}
-    }
-    protected void setUp() throws Exception {
-	//super.setUp();
-    }
-
-    protected void tearDown() throws Exception {
-	//super.tearDown();
-    }
-    */
     @Test
     public void testHello() throws Exception {
-	ClassifierClient c = new ClassifierClient("localhost", 9199, 10);
-	
-	
-	//	c.set_config("");
-	c.get_config("");
-	///	System.out.println("hello");
+	// ClassifierClient c = new ClassifierClient("localhost", 9199, 10);
+	// c.get_config("");
     }
     @Test
     public void testHello2(){
